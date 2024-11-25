efac0eef85948c23eafdfd74773adf6a7ad76d87
BrianChenNY <shunchao.chen@outlook.com>
2024-11-25 10:31:50 -0500

    Remove submodule and re-add frontend as a normal folder

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import "./login.scss";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Meet your Friends!</h1>
+          <p>Meet together to create a better world.</p>
+          <span>Don't you have an account?</span>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
+        </div>
+
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ frontend/src/pages/register/Register.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import "./register.scss";
+
+const Register = () => {
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Meet your Friends!</h1>
+          <p>Meet together to create a better world.</p>
+          <span>Already have an account?</span>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        </div>
+
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Name" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
