1a0c8ade09595e044a74665f8eb404f6e568e8bc
BrianChenNY <shunchao.chen@outlook.com>
2025-03-03 10:35:57 -0500

    Updated frontend 2.0

--- frontend/src/components/posts/Posts.jsx
+++ frontend/src/components/posts/Posts.jsx
@@ -6,28 +6,30 @@ const Posts = () => {
   const posts = [
     {
       id: 1,
-      name: "John Doe",
+      name: "JOJO DAI",
       userId: 1,
       profilePic:
         "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Hello World!",
       img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
     },
     {
       id: 2,
-      name: "Jane Doe",
+      name: "Kelly Chan",
       userId: 2,
       profilePic:
         "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      desc: "Tenetur iste voluptates dolorem rem commodi voluptate pariatur, voluptatum, laboriosam consequatur enim nostrum cumque! Maiores a nam non adipisci minima modi tempore.",
+      desc: "What a wonderful day! Be Happy Everyday!",
     },
   ];
 
-  return <div className="posts">
-    {posts.map(post=>(
-      <Post post={post} key={post.id}/>
-    ))}
-  </div>;
+  return (
+    <div className="posts">
+      {posts.map((post) => (
+        <Post post={post} key={post.id} />
+      ))}
+    </div>
+  );
 };
 
 export default Posts;
